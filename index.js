1dfd5d580de552a378e6c08fa74005d1876dc8a5
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2023-12-21 18:02:32 -0800

    all and single done

--- index.js
+++ index.js
@@ -2,6 +2,64 @@ const pg = require('pg')
 const client = new pg.Client('postgres://localhost/gameshop')
 const express = require('express')
 const app = express()
+//all videogames
+app.get('/api/videogames', async (req,res,next) => {
+    try {
+        const SQL = `
+            SELECT *
+            FROM videogames
+        `
+        const response = await client.query(SQL)
+        res.send(response.rows)
+
+    } catch (error) {
+        next(error)
+    }
+})
+//all boardgames
+app.get('/api/boardgames', async (req,res,next) => {
+    try {
+        const SQL = `
+            SELECT *
+            FROM boardgames
+        `
+        const response = await client.query(SQL)
+        res.send(response.rows)
+
+    } catch (error) {
+        next(error)
+    }
+})
+//single videogame
+app.get('/api/videogames/:id' , async (req,res,next) =>{
+    try{
+        console.log(req.params.id)
+        const SQL = `
+        select * from videogames WHERE id=$1   
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.send(response.rows)
+
+ 
+    } catch (error) {
+        next(error)
+    } 
+})
+//single boardgames
+app.get('/api/boardgames/:id' , async (req,res,next) =>{
+    try{
+        console.log(req.params.id)
+        const SQL = `
+        select * from boardgames WHERE id=$1   
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.send(response.rows)
+
+ 
+    } catch (error) {
+        next(error)
+    } 
+})
 
 const start = async () => {
     await client.connect()
