6feda7f2136359569c51e798e3c8277cc6dd26a7
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2023-12-29 10:15:14 -0800

    added post and put

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const pg = require('pg')
 const client = new pg.Client('postgres://localhost/gameshop')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 //all videogames
 app.get('/api/videogames', async (req,res,next) => {
     try {
@@ -33,7 +35,6 @@ app.get('/api/boardgames', async (req,res,next) => {
 //single videogame
 app.get('/api/videogames/:id' , async (req,res,next) =>{
     try{
-        console.log(req.params.id)
         const SQL = `
         select * from videogames WHERE id=$1   
         `
@@ -48,7 +49,6 @@ app.get('/api/videogames/:id' , async (req,res,next) =>{
 //single boardgames
 app.get('/api/boardgames/:id' , async (req,res,next) =>{
     try{
-        console.log(req.params.id)
         const SQL = `
         select * from boardgames WHERE id=$1   
         `
@@ -60,6 +60,94 @@ app.get('/api/boardgames/:id' , async (req,res,next) =>{
         next(error)
     } 
 })
+//Delete a videogames
+app.delete('/api/videogames/:id', async (req,res,next) => {
+    try {
+        const SQL = `
+        DELETE FROM videogames WHERE id=$1
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.sendStatus(204)
+
+    } catch (error) {
+        next(error)
+    }
+
+})
+//Delete a boardgames
+app.delete('/api/boardgames/:id', async (req,res,next) => {
+    try {
+        const SQL = `
+        DELETE FROM boardgames WHERE id=$1
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.sendStatus(204)
+
+    } catch (error) {
+        next(error)
+    }
+
+})
+//post videogames
+app.post('/api/videogames', async(req,res,next) => {
+    try {
+        const SQL =`
+           INSERT INTO videogames(name)
+           VALUES($1)
+           RETURNING *
+        `
+        const response = await client.query(SQL, [req.body.name])
+        res.send(response.rows)
+
+    } catch (error) {
+        next(error)
+    }
+})
+//post boardgames
+app.post('/api/boardgames', async(req,res,next) => {
+    try {
+        const SQL =`
+           INSERT INTO boardgames(name)
+           VALUES($1)
+           RETURNING *
+        `
+        const response = await client.query(SQL, [req.body.name])
+        res.send(response.rows)
+
+    } catch (error) {
+        next(error)
+    }
+})
+//update a single videogame/boardgame
+app.put('/api/videogames/:id', async (req,res,next) => {
+    try {
+       const SQL = `
+          UPDATE videogames
+          SET name = $1
+          WHERE id = $2
+          RETURNING *
+       `
+       const response = await client.query(SQL, [req.body.name, req.params.id])
+       res.send(response.rows)
+    }catch (error) {
+        next(error)
+    }
+})
+app.put('/api/boardgames/:id', async (req,res,next) => {
+    try {
+       const SQL = `
+          UPDATE boardgames
+          SET name = $1
+          WHERE id = $2
+          RETURNING *
+       `
+       const response = await client.query(SQL, [req.body.name, req.params.id])
+       res.send(response.rows)
+    }catch (error) {
+        next(error)
+    }
+})
+
 
 const start = async () => {
     await client.connect()
